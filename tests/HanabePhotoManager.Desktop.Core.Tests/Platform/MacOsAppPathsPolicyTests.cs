52bac591ea9a25668929be5d3927055c72b07362
Hanabe <fulian@outlook.com>
2026-07-26 22:02:28 +0800

    fix: normalize macOS application paths

--- tests/HanabePhotoManager.Desktop.Core.Tests/Platform/MacOsAppPathsPolicyTests.cs
+++ tests/HanabePhotoManager.Desktop.Core.Tests/Platform/MacOsAppPathsPolicyTests.cs
@@ -25,4 +25,53 @@ public sealed class MacOsAppPathsPolicyTests
 
         action.Should().Throw<ArgumentException>();
     }
+
+    [Fact]
+    public void Resolve_RejectsRelativeHome()
+    {
+        var action = () => MacOsAppPathsPolicy.Resolve("Users/hanabe");
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Resolve_NormalizesParentSegmentsWithinRoot()
+    {
+        var result = MacOsAppPathsPolicy.Resolve("/Users/hanabe/..");
+
+        result.ApplicationDataDirectory.Should()
+            .Be("/Users/Library/Application Support/Hanabe Photo Manager");
+        result.CacheDirectory.Should()
+            .Be("/Users/Library/Caches/Hanabe Photo Manager");
+    }
+
+    [Fact]
+    public void Resolve_RejectsHomeContainingNullCharacter()
+    {
+        var action = () => MacOsAppPathsPolicy.Resolve("/Users/hanabe\0malicious");
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Resolve_PreservesRootHome()
+    {
+        var result = MacOsAppPathsPolicy.Resolve("/");
+
+        result.ApplicationDataDirectory.Should()
+            .Be("/Library/Application Support/Hanabe Photo Manager");
+        result.CacheDirectory.Should()
+            .Be("/Library/Caches/Hanabe Photo Manager");
+    }
+
+    [Fact]
+    public void Resolve_ClampsParentSegmentsAtRoot()
+    {
+        var result = MacOsAppPathsPolicy.Resolve("/../Users");
+
+        result.ApplicationDataDirectory.Should()
+            .Be("/Users/Library/Application Support/Hanabe Photo Manager");
+        result.CacheDirectory.Should()
+            .Be("/Users/Library/Caches/Hanabe Photo Manager");
+    }
 }
